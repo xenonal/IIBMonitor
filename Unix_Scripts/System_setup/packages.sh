8cfd98d51304deb2b6f7aba8da43efa2c8a7bef0
James McVicar <jamesmcvicar1@btinternet.com>
2017-08-21 22:34:33 +0100

    COMMIT FILES

--- Unix_Scripts/System_setup/packages.sh
+++ Unix_Scripts/System_setup/packages.sh
@@ -19,10 +19,31 @@
 #
 # Method:       manually as required.
 # Usage:        packages.sh from within OS_Setup.sh
+#               command takes an external comma seperated list of packages to install
 #
 # Description:  Used to to install software needed by the OS or docker image
 #
 #
+#Future  enhancement would be to work out if the OS is debian or RHEL based for yum installs
+#also the ability to update the list of repos incase packages are not standard.
 #------------------------------------------------------------------------------------------
+echo "Installing requested packaages"
+
+#updating the packages list
+
+echo "Updating repository lists"
 apt-get update -y
-apt-get install -y bash bc curl rpm tar
+
+#Use the external arguments to create a directories in the same path
+
+printf '%s' "$1"  | xargs -d, -I '{}' echo apt-get install -y '{}'
+printf '%s' "$1"  | xargs -d, -I '{}' apt-get install -y '{}'
+
+#deterimes the exit code success carry on failure exit
+#0 = sucess
+#1 = failure
+RETVAL=$?
+[ $RETVAL -eq 0 ] && echo packages installed Successfully
+[ $RETVAL -ne 0 ] && echo Error installing packages && exit 1
+
+echo "Packages were installed successfully"
